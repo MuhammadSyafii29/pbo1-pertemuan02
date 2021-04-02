6f77643666dd8aff949e8fa884162b8a3580bd88
Muhammadsyafii29 <syafii.01.muhammad@gmail.com>
2021-04-02 17:11:40 +0800

    Memodifikasi File InputOutput

--- InputOutput.java
+++ InputOutput.java
@@ -4,8 +4,23 @@ public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
   String nama;
+  String Ttl;
+  String Hoby;
+  String Alamat;
+
   System.out.print("Masukkan nama anda: ");
   nama = scanner.nextLine();
-  System.out.println("Hai, "+nama);
+  System.out.print("Masukkan Tempat tanggal lahir anda: ");
+  Ttl = scanner.nextLine();
+  System.out.print("Masukkan Hoby anda: ");
+  Hoby = scanner.nextLine();
+  System.out.print("Masukkan Alamat anda: ");
+  Alamat = scanner.nextLine();
+
+  System.out.println("==============================");
+  System.out.println("Nama anda :  "+nama);
+  System.out.println("Tempat tanggal lahir anda : "+Ttl);
+  System.out.println("Hoby anda : "+Hoby);
+  System.out.println("Alamat anda : "+Alamat);
  }
 }
